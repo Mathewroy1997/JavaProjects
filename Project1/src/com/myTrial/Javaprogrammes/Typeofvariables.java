bf98d9f17e852fff87a20b3603cc570d611dc4a5
unknown <mathewroy.k@gmail.com>
2021-02-05 11:54:58 +0530

    New class 'typeof variables' added.

---
+++ Project1/src/com/myTrial/Javaprogrammes/Typeofvariables.java
@@ -0,0 +1,23 @@
+package com.myTrial.Javaprogrammes;
+
+public class Typeofvariables {
+    public static void main(String[] args) {
+        int myInt = 10;
+        short myShort = 554;
+        long myLong = 49965;
+
+        float myFloat = 15.2f;
+        double myDouble = 15.155;
+
+        char myChar = 'a';
+        boolean myBoolean = true;
+
+        System.out.println(myInt);
+        System.out.println(myBoolean);
+        System.out.println(myChar);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myLong);
+        System.out.println(myShort);
+    }
+}
