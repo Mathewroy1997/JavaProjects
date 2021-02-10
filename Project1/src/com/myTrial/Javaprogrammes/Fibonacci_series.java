bbd5e4fd1d3eadf3be95b7efb4bf7d1068b9d1d1
unknown <mathewroy.k@gmail.com>
2021-02-10 20:16:55 +0530

    Fibonacci series- programming started

---
+++ Project1/src/com/myTrial/Javaprogrammes/Fibonacci_series.java
@@ -0,0 +1,24 @@
+package com.myTrial.Javaprogrammes;
+
+import java.util.Scanner;
+
+public class Fibonacci_series {
+    public static void main(String[] args) {
+        int i, j=0,k=1;
+        Scanner elements = new Scanner(System.in);
+        System.out.println("Enter no. of elements (can't be zero):");
+        i=elements.nextInt();
+        if(i==1)
+        {
+            System.out.printf("Fibonacci Series =%d "&j);
+
+        }
+        else if(i==2)
+        {
+            System.out.printf("Fibonacci Series =%d %d" &k);
+        }
+        else{
+            for()
+        }
+    }
+}
