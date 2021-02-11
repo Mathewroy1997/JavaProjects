33d1f14fba516e5d334aafe316415615f255f33f
unknown <mathewroy.k@gmail.com>
2021-02-11 09:23:32 +0530

    Fibonacci_series updated.

--- Project1/src/com/myTrial/Javaprogrammes/Fibonacci_series.java
+++ Project1/src/com/myTrial/Javaprogrammes/Fibonacci_series.java
@@ -4,21 +4,32 @@ import java.util.Scanner;
 
 public class Fibonacci_series {
     public static void main(String[] args) {
-        int i, j=0,k=1;
+        int i, j=0,k=1,l,temp;
         Scanner elements = new Scanner(System.in);
         System.out.println("Enter no. of elements (can't be zero):");
         i=elements.nextInt();
         if(i==1)
         {
-            System.out.printf("Fibonacci Series =%d "&j);
+            System.out.printf("Fibonacci Series =%d ",j);
+
 
         }
         else if(i==2)
         {
-            System.out.printf("Fibonacci Series =%d %d" &k);
+            System.out.printf("Fibonacci Series =%d ",j);
+            System.out.printf("%d ",k);
         }
         else{
-            for()
+            System.out.printf("Fibonacci Series =%d ",j);
+            System.out.printf("%d ",k);
+
+            for(l=0;l<i-2;l++){
+                temp=k;
+                k=j+k;
+                j=temp;
+                System.out.printf("%d ",k);
+
+            }
         }
     }
 }
