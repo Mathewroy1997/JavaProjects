ca9aaf6337606e48650a4253bd61f2cb1c91d368
unknown <mathewroy.k@gmail.com>
2021-02-12 09:11:04 +0530

    Reverse of a number programme completed.

--- Project1/src/com/myTrial/Javaprogrammes/Reverse_number.java
+++ Project1/src/com/myTrial/Javaprogrammes/Reverse_number.java
@@ -1,7 +1,24 @@
 package com.myTrial.Javaprogrammes;
 
+import java.util.Scanner;
+
 public class Reverse_number {
     public static void main(String[] args ){
+        int number,temp1,temp2;
+        int reverse=0;
+        Scanner integer=new Scanner(System.in);
+        System.out.println("Enter the integer :");
+        number=integer.nextInt();
+        temp1=number;
+        do{
+            temp2=temp1%10;
+            reverse=(reverse*10)+temp2;
+            temp1=temp1/10;
+            if(temp1<10){
+                reverse=(reverse*10)+temp1;
+            }
 
+        }while(temp1>10);
+        System.out.printf("Reverse of the number is : %d",reverse);
     }
 }
